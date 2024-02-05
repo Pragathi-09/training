81805b52e9cf7fa869a4dd0241852c9aec137b2d
Kasoju Pragathi Balakishan <kasoju.balakishan@kyndryl.com>
2024-02-05 22:03:00 +0530

    this is my second update

---
+++ file1.py
@@ -0,0 +1,8 @@
+About your profile
+Your profile page tells people the story of your work through the repositories you're interested in, the contributions you've made, and the conversations you've had.
+
+About your organization's profile
+Your organization's profile page shows basic information about your organization.
+
+Personalizing your profile
+You can share information about yourself with other GitHub users by setting a profile picture and adding a bio to your profile.
